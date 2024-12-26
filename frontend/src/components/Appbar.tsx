b5abe3d9d6ddf82ccf5ed5d2947bc3c20ea6be1f
iamindranil <indranilchakraborty692@gmail.com>
2024-12-26 20:15:49 +0530

    some changes

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Avatar } from './BlogCard'
 
 const Appbar = ({shouldPublish}:{shouldPublish:boolean}) => {
